125351096e4e5c65ea58e7d2091cec7dffb0abf3
Techsms <Sakshi221310@gmail.com>
2024-08-06 14:11:56 +0530

    Add files via upload

---
+++ _1Print_array.java
@@ -0,0 +1,13 @@
+public class _1Print_array {
+    static void print(){
+        int arr[]={1,2,3,4,5};
+    for(int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
+      
+    }
+    }
+    public static void main(String[] args) {
+         //_1Print_array obj= new _1Print_array();
+        print();
+            }
+}
