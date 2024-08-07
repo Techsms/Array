bfe82de7551c3dc79f79d0421bf5281330d5eddd
Sakshi Gupta <Sakshi221310@gmail.com>
2024-08-07 13:45:28 +0530

    Add files via upload

---
+++ Arra_Sum.java
@@ -0,0 +1,16 @@
+public class Arra_Sum{
+public static void main(String[] args) {
+    int arr[]={12,15,6,21,8};
+int sum=0;
+for(int i= 0;i<arr.length;i++){
+    sum=sum+arr[i];
+}
+System.out.print(sum);
+
+}
+
+
+
+
+
+}
