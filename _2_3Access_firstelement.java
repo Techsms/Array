e1b2ff5e8a3c9a05cd6f713c85b2718ce1015b36
Sakshi Gupta <Sakshi221310@gmail.com>
2024-08-08 14:15:27 +0530

    Add files via upload

---
+++ _2_3Access_firstelement.java
@@ -0,0 +1,8 @@
+public class _2_3Access_firstelement{
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        int a=arr.length-1;
+        System.out.println(arr[0]);
+        System.out.println(arr[a]);
+    }
+}
