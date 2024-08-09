05076e360997f4ae16f4d95df009675bb7ecaef7
Sakshi Gupta <Sakshi221310@gmail.com>
2024-08-09 14:35:04 +0530

    Add files via upload

---
+++ _4Array.java
@@ -0,0 +1,12 @@
+public class _4Array {
+    public static void main(String[] args) {
+        int arr[]= {7,14,21,28};
+        int count= 0;
+        for(int i=0;i<arr.length;i++){
+            count ++;
+        }
+        System.out.println(count);
+          
+        
+    }
+}
